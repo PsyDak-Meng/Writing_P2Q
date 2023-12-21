ef3c8d9588196a371a2200b5d634b084c62b6e2d
PsyDak-Meng <105434864+PsyDak-Meng@users.noreply.github.com>
2023-12-21 10:11:51 -0500

    Add files via upload

--- modeling.py
+++ modeling.py
@@ -118,7 +118,7 @@ class P2Q(nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super(P2Q, self).__init__()
         self.input_dim = input_dim
-        self.transformer = nn.Transformer(d_model=90,nhead=6,
+        self.transformer = nn.Transformer(d_model=90,nhead=9,
                                           num_encoder_layers=3,num_decoder_layers=3, 
                                           dim_feedforward=1024, dropout=0.1, activation="relu",
                                           layer_norm_eps=1e-05, 
